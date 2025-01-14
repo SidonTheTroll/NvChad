323ade674645151a1381a7ad65add0859711bfe0
Sidon_OO0 <70560640+SidonTheTroll@users.noreply.github.com>
2025-01-14 12:46:10 +0000

    Update init.lua

--- lua/nvchad/plugins/init.lua
+++ lua/nvchad/plugins/init.lua
@@ -107,7 +107,7 @@ return {
       {
         -- snippet plugin
         "L3MON4D3/LuaSnip",
-        dependencies = "rafamadriz/friendly-snippets",
+        dependencies = "SidonTheTroll/friendly-snippets",
         opts = { history = true, updateevents = "TextChanged,TextChangedI" },
         config = function(_, opts)
           require("luasnip").config.set_config(opts)
